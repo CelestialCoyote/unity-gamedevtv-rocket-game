691961f1e7618d424d4d1576c00fbcc8546bfc6b
Paul Stearns <celestialcoyote@gmail.com>
2023-05-10 17:49:23 -0700

    added next level

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -13,6 +13,7 @@ public class CollisionHandler : MonoBehaviour
 				break;
 			case "Finish":
 				Debug.Log("Congrats you finished the level.");
+				LoadNextLevel();
 				break;
 			case "Fuel":
 				Debug.Log("This thing is fuel.");
@@ -27,7 +28,18 @@ public class CollisionHandler : MonoBehaviour
 	void ReloadLevel()
 	{
 		int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
-		
+
 		SceneManager.LoadScene(currentSceneIndex);
+	}
+
+	void LoadNextLevel()
+	{
+		int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+		int nextSceneIndex = currentSceneIndex + 1;
+
+		if (nextSceneIndex == SceneManager.sceneCountInBuildSettings)
+			nextSceneIndex = 0;
+		
+		SceneManager.LoadScene(nextSceneIndex);
 	}
 }
